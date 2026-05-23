721dd851c066f90f77942b69c974b737f493a869
jauhar7 <jauharmuf@gmail.com>
2026-05-23 11:23:57 +0700

    create halaman login

---
+++ src/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+/* untuk roting */
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
