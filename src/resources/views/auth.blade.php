a6c12f26202e8465c9e43d538c14fbd47fc180e5
Frezga <frezga3@gmail.com>
2026-06-06 16:42:02 +0700

    Update ukuran icon dan  beberapa styling

--- src/resources/views/auth.blade.php
+++ src/resources/views/auth.blade.php
@@ -12,6 +12,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SpotRent {{ $initialMode === 'register' ? 'Register' : 'Login' }}</title>
+    <!-- Website Icon -->
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
